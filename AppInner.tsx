35c18c856f682791318a8a721292cdd803bb6f80
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-10-26 00:57:18 +0900

    Folder structure

---
+++ AppInner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AppInner = (): React.JSX.Element => {
+  return (
+    <View>
+      <Text>sss</Text>
+    </View>
+  );
+};
+
+export default AppInner;
